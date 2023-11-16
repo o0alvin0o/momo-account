c946c7f518a8fe4f65e712def4176de9a4484262
Cuong Vo <cuongvo.quoc09@gmail.com>
2023-11-16 22:12:05 +0700

    hotfix(NO-JIRA): add dependency for CommandDispatcher

--- app/src/main/java/com/momodev/App.java
+++ app/src/main/java/com/momodev/App.java
@@ -1,16 +1,20 @@
 package com.momodev;
 
 import com.momodev.constants.Command;
+import com.momodev.service.AccountService;
 import com.momodev.service.CommandDispatcher;
 
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, IOException {
         if (args.length == 0) {
             Command command = new Command();
             command.printCommandList();
             return;
         }
         CommandDispatcher dispatcher = new CommandDispatcher();
+        dispatcher.setAccountService(AccountService.getDefault());
         dispatcher.dispatch(args);
     }
 }
--- app/src/main/java/com/momodev/service/AccountService.java
+++ app/src/main/java/com/momodev/service/AccountService.java
@@ -2,12 +2,18 @@ package com.momodev.service;
 
 import com.momodev.models.Bill;
 import com.momodev.models.Payment;
+import com.momodev.service.impl.SimpleAccountService;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface AccountService {
+
+    static AccountService getDefault() throws IOException {
+        return new SimpleAccountService(DataOperator.getDefault());
+    }
 
     void deposit(BigDecimal amount);
 
