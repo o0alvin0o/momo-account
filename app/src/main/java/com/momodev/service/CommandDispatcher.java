d5865a9afdfaa85716fbf78cc4c7c5e239602732
Cuong Vo <cuongvo.quoc09@gmail.com>
2023-11-16 21:46:03 +0700

    feat(JIRA-5): implement CommandDispatcher service

--- app/src/main/java/com/momodev/service/CommandDispatcher.java
+++ app/src/main/java/com/momodev/service/CommandDispatcher.java
@@ -1,11 +1,23 @@
 package com.momodev.service;
 
+import com.momodev.constants.AppConfig;
 import com.momodev.constants.AppMessage;
 import com.momodev.constants.Command;
+import com.momodev.enums.BillState;
+import com.momodev.models.Account;
 import com.momodev.models.ActiveRecord;
+import com.momodev.models.Bill;
+import com.momodev.models.Payment;
+import com.momodev.utils.AppUtils;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class CommandDispatcher {
 
@@ -32,43 +44,128 @@ public class CommandDispatcher {
     }
 
     private void getBalance() {
+        try {
+            System.out.println("You have: " + accountService.getBalance(AppConfig.DEFAULT_ACCOUNT_ID));
+        } catch (Exception exception) {
+            System.out.println(AppMessage.ERR_READ_DATA);
+        }
     }
 
     private void searchBillBy(String[] values) {
+        if (values.length < 2) {
+            System.out.println(AppMessage.INVALID_SEARCH_BILL_ARGS);
+            return;
+        }
+        List<Bill> bills = accountService.searchBillBy(values[0], values[1]);
+
+        printResult(bills, Bill.class);
     }
 
     private void dueDate() {
+        List<Bill> bills = accountService.dueDate();
+        printResult(bills, Bill.class);
     }
 
     private void schedule(String[] values) {
+        int hour;
+        int minute;
+        try {
+            String billId = values[0];
+            LocalDate scheduleDate = LocalDate.parse(values[1], DateTimeFormatter.ISO_LOCAL_DATE);
+            if (values[2].length() != 5 || values[2].charAt(2) != ':') {
+                System.out.println(AppMessage.ENTER_VALID_TIME);
+                return;
+            }
+            hour = Integer.parseInt(values[2].substring(0, 2));
+            minute = Integer.parseInt(values[2].substring(3, 5));
+            LocalDateTime scheduleTime = scheduleDate.atTime(hour, minute);
+            accountService.schedule(billId, scheduleTime);
+        } catch (NumberFormatException ex) {
+            System.out.println(AppMessage.ENTER_VALID_TIME);
+        } catch (DateTimeParseException exception) {
+            System.out.println(AppMessage.ENTER_VALID_DATE);
+        }
     }
 
     private void listPayment() {
+        List<Payment> payments = accountService.listPayment();
+        printResult(payments, Payment.class);
     }
 
     private void listBill() {
+        List<Bill> bills = accountService.listBill();
+        printResult(bills, Bill.class);
     }
 
     private void withdraw(String value) {
+        BigDecimal amount;
+        try {
+            amount = new BigDecimal(value);
+            accountService.withdraw(amount);
+        } catch (NumberFormatException ex) {
+            System.out.println(AppMessage.ENTER_VALID_NUMBER);
+        }
     }
 
     private void deposit(String value) {
+        BigDecimal amount;
+        try {
+            amount = new BigDecimal(value);
+            accountService.deposit(amount);
+        } catch (NumberFormatException ex) {
+            System.out.println(AppMessage.ENTER_VALID_NUMBER);
+        }
     }
 
     private void pay(String[] values) {
+        accountService.pay(Arrays.asList(values));
     }
 
     private void generateTestData() {
+        try {
+            Account account = Account.init();
+
+            Bill electricBill = new Bill("Electric", new BigDecimal(500000),
+                    LocalDate.of(2023,12,15), BillState.NOT_PAID, "EVN HCM");
+            Bill waterBill = new Bill("Water", new BigDecimal(100000),
+                    LocalDate.of(2023,12,10), BillState.NOT_PAID, "TRUNG AN");
+            Bill fptBill = new Bill("Internet", new BigDecimal(200000),
+                    LocalDate.of(2023,11,29), BillState.NOT_PAID, "FPT");
+            Bill viettelBill = new Bill("Internet", new BigDecimal(200000),
+                    LocalDate.of(2023,11,28), BillState.NOT_PAID, "VIETTEL");
+
+            Bill dueBill1 = new Bill("Test due 1", new BigDecimal(80000),
+                    LocalDate.of(2023,10,28), BillState.NOT_PAID, "DUE1");
+            Bill dueBill2 = new Bill("Test due 2", new BigDecimal(20000),
+                    LocalDate.of(2023,9,15), BillState.NOT_PAID, "DUE2");
+
+            DataOperator dataOperator = DataOperator.getDefault();
+            try {
+                dataOperator.persist(account, Account.class);
+                dataOperator.persist(List.of(electricBill, waterBill, fptBill, viettelBill, dueBill1, dueBill2), Bill.class);
+            } catch (Exception ex) {
+                System.out.println(AppMessage.ERR_READ_DATA);
+            }
+
+        } catch (Exception exception) {
+            System.out.println(AppMessage.ERR_READ_DATA);
+        }
+
     }
 
     private <T extends ActiveRecord> void printResult(List<T> records, Class clazz) {
+        AppUtils.printHeaders(clazz);
+        if (records.isEmpty()) {
+            return;
+        }
+        AppUtils.printRecords(records, clazz);
     }
     private void exitApp() {
-        System.out.print(AppMessage.EXIT);
+        System.out.println(AppMessage.EXIT);
     }
 
     private void notSupportedCommand() {
-        System.out.print(AppMessage.NOT_SUPPORTED);
+        System.out.println(AppMessage.NOT_SUPPORTED);
     }
 
     public void setAccountService(AccountService accountService) {
@@ -76,5 +173,6 @@ public class CommandDispatcher {
     }
 
     public void init() {
+        accountService.init();
     }
 }
